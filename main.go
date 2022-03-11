6ff9c27cf1345e398cfd345a4de2726d31f76a9a
Whyzen <girard.paul39@gmail.com>
2022-03-11 16:19:02 +0100

    make it better

--- main.go
+++ main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"test-go-project/database"
+	_ "test-go-project/docs/testproject"
 	"test-go-project/testcontroller"
 )
 
-func apiRoutes(app *fiber.App) {
+// @title Fiber Swagger Example API
+// @version 2.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3001
+// @BasePath /
+// @schemes http
+func main() {
+	app := fiber.New()
 	app.Get("/api/test", testcontroller.GetAll)
 	app.Get("/api/test/:id", testcontroller.GetOne)
 	app.Post("/api/test", testcontroller.Create)
 	app.Put("/api/test/:id", testcontroller.UpdateOne)
 	app.Delete("/api/test/:id", testcontroller.RemoveOne)
-}
-
-func main() {
-	app := fiber.New()
-
-	apiRoutes(app)
+	app.Get("/swagger/*", swagger.HandlerDefault)
 	database.ConnectDB()
 	app.Listen(":3001")
 }
--- testcontroller/testcontroller.go
+++ testcontroller/testcontroller.go
@@ -1,86 +1,134 @@
 package testcontroller
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
-	"test-go-project/database"
-	test "test-go-project/model"
+  "github.com/gofiber/fiber/v2"
+  "github.com/google/uuid"
+  "test-go-project/database"
+  test "test-go-project/model"
 )
 
+// GetAll
+// @Summary GetAll.
+// @Description GetAll.
+// @Tags test
+// @Accept */*
+// @Produce json
+// @Success      200  {array}  test.Test
+// @Router /api/test [get]
 func GetAll(c *fiber.Ctx) error {
-	db := database.DB
-	var test []test.Test
+  db := database.DB
+  var test []test.Test
 
-	db.Find(&test)
+  db.Find(&test)
 
-	if len(test) == 0 {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "No test present", "data": nil})
-	}
+  if len(test) == 0 {
+    return c.Status(404).JSON(fiber.Map{"status": "error", "message": "No test present", "data": nil})
+  }
 
-	return c.JSON(fiber.Map{"status": "success", "message": "Test Found", "data": test})
+  return c.JSON(fiber.Map{"status": "success", "message": "Test Found", "data": test})
 }
+
+// GetOne
+// @Summary GetOne.
+// @Description GetOne.
+// @Tags test
+// @Accept */*
+// @Produce json
+// @Param id path string true "id"
+// @Success      200  {object}  test.Test
+// @Router /api/test/:id [get]
 func GetOne(c *fiber.Ctx) error {
-	db := database.DB
-	var object test.Test
+  db := database.DB
+  var object test.Test
 
-	err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
+  err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
 
-	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Test not found", "data": nil})
-	}
+  if err != nil {
+    return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Test not found", "data": nil})
+  }
 
-	return c.JSON(fiber.Map{"status": "success", "message": "Test Found", "data": object})
+  return c.JSON(fiber.Map{"status": "success", "message": "Test Found", "data": object})
 
 }
+
+// Create
+// @Summary Create.
+// @Description Create.
+// @Tags test
+// @Accept */*
+// @Produce json
+// @Param test body test.Test true "Test"
+// @Success      200  {object}  test.Test
+// @Router /api/test [post]
 func Create(c *fiber.Ctx) error {
-	db := database.DB
-	test := new(test.Test)
+  db := database.DB
+  test := new(test.Test)
 
-	err := c.BodyParser(test)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
-	}
-	test.ID = uuid.New()
-	err = db.Create(&test).Error
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create note", "data": err})
-	}
+  err := c.BodyParser(test)
+  if err != nil {
+    return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
+  }
+  test.ID = uuid.New()
+  err = db.Create(&test).Error
+  if err != nil {
+    return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create note", "data": err})
+  }
 
-	return c.JSON(fiber.Map{"status": "success", "message": "Created Note", "data": test})
+  return c.JSON(fiber.Map{"status": "success", "message": "Created Note", "data": test})
 }
+
+// UpdateOne
+// @Summary UpdateOne.
+// @Description UpdateOne.
+// @Tags test
+// @Accept */*
+// @Produce json
+// @Param id path string true "id"
+// @Success      200  {object}  test.Test
+// @Router /api/test/:id [put]
 func UpdateOne(c *fiber.Ctx) error {
-	db := database.DB
-	object := new(test.Test)
+  db := database.DB
+  object := new(test.Test)
 
-	err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
+  err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
 
-	err = c.BodyParser(object)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
-	}
+  err = c.BodyParser(object)
+  if err != nil {
+    return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
+  }
 
-	err = db.Save(&object).Error
+  err = db.Save(&object).Error
 
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not update test", "data": err})
-	}
+  if err != nil {
+    return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not update test", "data": err})
+  }
 
-	return c.JSON(fiber.Map{"status": "success", "message": "Updated Test", "data": object})
+  return c.JSON(fiber.Map{"status": "success", "message": "Updated Test", "data": object})
 }
+
+// RemoveOne
+// @Summary RemoveOne.
+// @Description RemoveOne.
+// @Tags test
+// @Accept */*
+// @Produce json
+// @Param id path string true "id"
+// @Success      200  {object}  test.Test
+// @Router /api/test/:id [delete]
 func RemoveOne(c *fiber.Ctx) error {
-	db := database.DB
-	var object test.Test
+  db := database.DB
+  var object test.Test
 
-	err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
+  err := db.Find(&object, test.Test{ID: uuid.MustParse(c.Params("id"))}).Error
 
-	if object.ID == uuid.Nil {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Test not found", "data": nil})
-	}
+  if object.ID == uuid.Nil {
+    return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Test not found", "data": nil})
+  }
 
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not delete test", "data": err})
-	}
+  if err != nil {
+    return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not delete test", "data": err})
+  }
 
-	return c.JSON(fiber.Map{"status": "success", "message": "Deleted Test", "data": object})
+  return c.JSON(fiber.Map{"status": "success", "message": "Deleted Test", "data": object})
 
 }
